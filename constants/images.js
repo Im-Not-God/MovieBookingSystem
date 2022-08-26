4fcbfe7aa3c6baa5a70a794be883a3cd24f47da8
Chong Yao Jun <82208147+Im-Not-God@users.noreply.github.com>
2022-08-26 16:26:32 +0800

    Update images.js

--- constants/images.js
+++ constants/images.js
@@ -1,5 +1,14 @@
-const profile_photo = require("../assets/images/dummy_profile/profile_photo.png");
+const profile_photo = require("../assets/images/img_avatar.png");
+const blank_profile_photo = require("../assets/images/blank_profile_photo.png");
+const profile_background =require("../assets/images/profile_background.jpg")
+const ticketBorder =require("../assets/images/ticketBorder.png")
+const ticket_background =require("../assets/images/ticket_background.png")
+
 
 export default {
-    profile_photo
+    profile_photo,
+    profile_background,
+    blank_profile_photo,
+    ticketBorder,
+    ticket_background
 }
